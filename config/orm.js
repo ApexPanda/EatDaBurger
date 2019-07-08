3ac66f9f2f427adf637522d456cc00ea15a263db
Kayla Tryon <dawnka22@gmail.com>
2019-07-08 17:56:16 -0600

    mysql error gone

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,7 @@ var connection = require('./connection.js');
 
 var orm = {
     all: function(tableInput, cb){
-        connection.query('SELECT * FROM'+tableInput+';', function(err, result){
+        connection.query(`SELECT * FROM ${tableInput};`, function(err, result){
             if (err) throw err;
             cb(result)
         })
