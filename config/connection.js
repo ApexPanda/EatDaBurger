872c7862f526d241adb345ad872bf6a70f1616b3
Kayla Tryon <dawnka22@gmail.com>
2019-07-10 19:09:10 -0600

    jawsdb added to connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db'
-})
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    })
+};
 
 connection.connect(function(err){
     if(err)throw err;
